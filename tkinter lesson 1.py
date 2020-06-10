2ae1ad48d42eb892ed6116a01100453375b203c5
Doug <douglasnavarrete10@gmail.com>
2020-06-09 21:51:26 -0600

    introduction to Tkinter

---
+++ tkinter lesson 1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+raiz= Tk()
+raiz.title("Prueba sabrosa")
+raiz.resizable(False, False)
+raiz.iconbitmap("bat_chino.ico")
+raiz.geometry("450x450")
+raiz.config(bg="pink" )
+raiz.mainloop()
